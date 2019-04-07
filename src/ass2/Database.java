67a784cd670f36f9f75ba634f5ba11284716188a
freeyamistry <z5160898@ad.unsw.edu.au>
2019-04-07 22:31:32 +1000

    Database and login screen created

---
+++ src/ass2/Database.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ass2;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Database {
+
+    public static Connection conn;
+
+    public static void openConnection() {
+
+        if (conn == null) {
+            try {
+                conn = DriverManager.getConnection("jdbc:sqlite:fitfans.db");
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static void createUserTable() throws SQLException {
+        openConnection();
+        Statement st = conn.createStatement();
+
+        String createStatement = "CREATE TABLE IF NOT EXISTS User "
+                + "(USERNAME TEXT PRIMARY KEY,"
+                + "PASSWORD TEXT NOT NULL,"
+                + "GENDER TEXT NOT NULL,"
+                + "DOB TEXT NOT NULL"
+                + ");";
+
+        st.execute(createStatement);
+    }
+
+    public static void createFoodTable() throws SQLException {
+        openConnection();
+        Statement st = conn.createStatement();
+
+        String createStatement = "CREATE TABLE IF NOT EXISTS Food "
+                + "(USERNAME TEXT PRIMARY KEY,"
+                + "DATE TEXT NOT NULL,"
+                + "KILOJOULES INTEGER NOT NULL,"
+                + "FOODGROUP TEXT NOT NULL"
+                + ");";
+
+        st.execute(createStatement);
+    }
+
+    public static void createGymTable() throws SQLException {
+        openConnection();
+        Statement st = conn.createStatement();
+
+        String createStatement = "CREATE TABLE IF NOT EXISTS Gym "
+                + "(USERNAME TEXT PRIMARY KEY,"
+                + "ATTENDANCE TEXT NOT NULL,"
+                + "DATE TEXT NOT NULL,"
+                + "TIMESPENT INTEGER NOT NULL"
+                + ");";
+
+        st.execute(createStatement);
+    }
+
+    public static void createWeightTable() throws SQLException {
+        openConnection();
+        Statement st = conn.createStatement();
+
+        String createStatement = "CREATE TABLE IF NOT EXISTS Weight "
+                + "(USERNAME TEXT PRIMARY KEY,"
+                + "WEIGHT TEXT NOT NULL,"
+                + "DATE TEXT NOT NULL,"
+                + ");";
+
+        st.execute(createStatement);
+    }
+
+    public static void createGoalsTable() throws SQLException {
+        openConnection();
+        Statement st = conn.createStatement();
+
+        String createStatement = "CREATE TABLE IF NOT EXISTS Goals "
+                + "(USERNAME TEXT PRIMARY KEY,"
+                + "KILOJOULESPERDAY TEXT NOT NULL,"
+                + "WEIGHT TEXT NOT NULL,"
+                + "GYMPERWEKK INTEGER NOT NULL"
+                + ");";
+
+        st.execute(createStatement);
+    }
+
+}

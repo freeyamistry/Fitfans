0981d1c44f5e12df4b4695c24614278bfaecac9d
Jenney Le <jenneyle@Jenneys-MacBook-Air.local>
2019-04-10 11:53:37 +1000

    hi

--- src/Fitfans/src/fitfans/LoginScreen.java
+++ src/Fitfans/src/fitfans/LoginScreen.java
@@ -46,6 +46,7 @@ public class LoginScreen extends Application {
         System.out.println("i hate acct");
         System.out.println("hi i hate my life ");
         System.out.println("yo");
+        System.out.println("hiiii");
 
     }
     
